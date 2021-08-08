9ddb419dcfa860b425f121f25e436d7fad3cb727
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-08 19:23:42 -0400

    switched c to h: HUMAN ERROR

---
+++ cenv.h
@@ -0,0 +1,15 @@
+##ifndef C_ENV_H
+#define C_ENV_H
+typedef struct ENV_STRUCT
+{
+    enum
+    {
+        ENV_PATH,
+        ENV_PS_MODULE_PATH
+    } ENV_Type;
+    char type[];
+} ENT_t;
+
+ENV_t* set_env(char type[], char value[] );
+
+##endif
